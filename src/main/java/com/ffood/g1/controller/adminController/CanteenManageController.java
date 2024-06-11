8d215e1f93eb20c007d70baab784d3be48763c44
vux <vucqhe150798@fpt.edu.vn>
2024-06-11 20:29:23 +0700

    divide folder Management

---
+++ src/main/java/com/ffood/g1/controller/adminController/CanteenManageController.java
@@ -0,0 +1,71 @@
+package com.ffood.g1.controller.adminController;
+
+import com.ffood.g1.entity.Canteen;
+
+import com.ffood.g1.service.CanteenService;
+import com.ffood.g1.service.RoleService;
+import com.ffood.g1.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+public class CanteenManageController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private CanteenService canteenService;
+
+
+    @GetMapping("/manage-canteen")
+    public String listCanteens(Model model,
+                               @RequestParam(value = "page", defaultValue = "0") int page,
+                               @RequestParam(value = "size", defaultValue = "10") int size) {
+        Page<Canteen> canteenPage = canteenService.getAllCanteensPage(page, size);
+        model.addAttribute("canteenPage", canteenPage);
+        return "./admin-management/manage-canteen";
+    }
+
+    @GetMapping("/search-canteen")
+    public String searchCanteens(Model model,
+                                 @RequestParam(value = "page", defaultValue = "0") int page,
+                                 @RequestParam(value = "size", defaultValue = "10") int size,
+                                 @RequestParam(value = "keyword", required = false) String keyword) {
+        Page<Canteen> canteenPage;
+
+        if (keyword == null || keyword.isEmpty()) {
+            canteenPage = canteenService.getAllCanteensPage(page, size);
+        } else {
+            canteenPage = canteenService.searchCanteens(keyword, page, size);
+        }
+        model.addAttribute("canteenPage", canteenPage);
+        model.addAttribute("keyword", keyword);
+
+        return "./admin-management/manage-canteen";
+    }
+
+
+    @GetMapping("/add-canteen")
+    public String showAddCanteenForm(Model model) {
+        model.addAttribute("canteen", new Canteen());
+        model.addAttribute("managers", userService.getAllManagers());
+        return "./admin-management/add-canteen";
+    }
+
+    @PostMapping("/add-canteen")
+    public String addCanteen(@ModelAttribute("canteen") Canteen canteen, Model model) {
+        canteenService.saveCanteen(canteen);
+        return "redirect:/manage-canteen";
+    }
+
+
+
+
+}
--- src/main/java/com/ffood/g1/controller/adminController/UserManageController.java
+++ src/main/java/com/ffood/g1/controller/adminController/UserManageController.java
@@ -1,9 +1,7 @@
 package com.ffood.g1.controller.adminController;
 
-import com.ffood.g1.entity.Canteen;
 import com.ffood.g1.entity.Role;
 import com.ffood.g1.entity.User;
-import com.ffood.g1.service.CanteenService;
 import com.ffood.g1.service.RoleService;
 import com.ffood.g1.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,17 +13,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-public class ManageController {
+public class UserManageController {
+
     @Autowired
     private UserService userService;
 
     @Autowired
     private RoleService roleService;
-
-    @Autowired
-    private CanteenService canteenService;
-
-
     @GetMapping("/manage-user")
     public String listUsers(Model model,
                             @RequestParam(value = "page", defaultValue = "0") int page,
@@ -92,50 +86,5 @@ public class ManageController {
         userService.saveUser(user);
         return "redirect:/manage-user";
     }
-
-
-    @GetMapping("/manage-canteen")
-    public String listCanteens(Model model,
-                               @RequestParam(value = "page", defaultValue = "0") int page,
-                               @RequestParam(value = "size", defaultValue = "10") int size) {
-        Page<Canteen> canteenPage = canteenService.getAllCanteensPage(page, size);
-        model.addAttribute("canteenPage", canteenPage);
-        return "./admin-management/manage-canteen";
-    }
-
-    @GetMapping("/search-canteen")
-    public String searchCanteens(Model model,
-                                 @RequestParam(value = "page", defaultValue = "0") int page,
-                                 @RequestParam(value = "size", defaultValue = "10") int size,
-                                 @RequestParam(value = "keyword", required = false) String keyword) {
-        Page<Canteen> canteenPage;
-
-        if (keyword == null || keyword.isEmpty()) {
-            canteenPage = canteenService.getAllCanteensPage(page, size);
-        } else {
-            canteenPage = canteenService.searchCanteens(keyword, page, size);
-        }
-        model.addAttribute("canteenPage", canteenPage);
-        model.addAttribute("keyword", keyword);
-
-        return "./admin-management/manage-canteen";
-    }
-
-
-    @GetMapping("/add-canteen")
-    public String showAddCanteenForm(Model model) {
-        model.addAttribute("canteen", new Canteen());
-        model.addAttribute("managers", userService.getAllManagers());
-        return "./admin-management/add-canteen";
-    }
-
-    @PostMapping("/add-canteen")
-    public String addCanteen(@ModelAttribute("canteen") Canteen canteen, Model model) {
-        canteenService.saveCanteen(canteen);
-        return "redirect:/manage-canteen";
-    }
-
-
-
 
 }
